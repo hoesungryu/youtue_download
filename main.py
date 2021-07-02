5fd8fbb9b3bbd446332133d7eb1bd2597ea56958
hoesungryu <hoesungryu@korea.ac.kr>
2021-07-02 20:11:08 +0900

    mainfix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os 
 import time
-import tqdm
+from tqdm import tqdm
 from datetime import datetime
-import argpars
+import argparse
 
 # user_defined function
 from utils.utils import downloadYouTube
@@ -10,26 +10,25 @@ from utils.utils import downloadYouTube
 # arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--txt", type=str,required=True, help='.txt file path')
-parser.add_argument("--save_path", type=str,required=False, help='save path')
+parser.add_argument("--save_path", type=str,required=False, default='./youtube_video', help='save path')
 args = parser.parse_args()
 
 
 def main(args):
     start = time.time()
     date = datetime.now()
-    
-	youtube_txt = args.txt
-	save_path = args.save_path
 
+    txt = args.txt
+    save_path = args.save_path
+
+    youtube_txt = open(txt,"r+") 
     lines = youtube_txt.readlines()
-    print('[INFO] START Download {:02d}M{:02d}D_{:02d}h{:02d}m'.format(date.month, date.day, date.hour, date.minute)
-
+    print('[INFO] START Download {:02d}M{:02d}D_{:02d}h{:02d}m'.format(date.month, date.day, date.hour, date.minute))
     for line in tqdm(lines):
         downloadYouTube(line, save_path)
-        
+    
     end = time.time()
     print(f"[INFO]| Done ... \nTotal duration:{end_time - start_time}")
     
-
-if __name__=='__main__':
-	main(args)
+if __name__== "__main__":
+    main(args)
